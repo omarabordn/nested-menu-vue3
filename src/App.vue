e585be7313e9faa223986c49b7112c8218f9a0d4
Omar Abordn <omar.abordn@gmail.com>
2024-09-04 11:26:33 +0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="flex">
+    <!-- Sidebar with pages -->
+    <div class="w-1/4 p-4 border-r">
+      <h2 class="font-bold mb-4">Pages</h2>
+      <ul>
+        <li
+          v-for="page in pages"
+          :key="page.id"
+          @click="addPageToMenu(page)"
+          class="cursor-pointer p-2 bg-gray-100 mb-2 rounded hover:bg-gray-200"
+        >
+          {{ page.name }}
+        </li>
+      </ul>
+    </div>
+
+    <!-- Menu Builder -->
+    <div class="w-3/4 p-4">
+      <h2 class="font-bold mb-4">Menu Builder</h2>
+      <ul id="menu-list" class="p-4 border border-gray-300 rounded bg-white">
+        <MenuItem
+          v-for="(item, index) in menu"
+          :key="item.id"
+          :item="item"
+          :index="index"
+          @remove-item="removeMenuItem"
+        />
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { defineComponent, reactive, onMounted } from 'vue';
+import MenuItem from './components/MenuItem.vue';
+import Sortable from 'sortablejs';
+
+interface Page {
+  id: number;
+  name: string;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+interface MenuItem {
+  id: number;
+  name: string;
+  children: MenuItem[];
 }
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    MenuItem,
+  },
+  setup() {
+    const pages = reactive<Page[]>([
+      { id: 1, name: 'Home' },
+      { id: 2, name: 'About Us' },
+      { id: 3, name: 'Services' },
+      { id: 4, name: 'Contact' },
+    ]);
+
+    const menu = reactive<MenuItem[]>([]);
+
+    const addPageToMenu = (page: Page) => {
+      menu.push({ id: Date.now(), name: page.name, children: [] });
+    };
+
+    const removeMenuItem = (index: number) => {
+      menu.splice(index, 1);
+    };
+
+    onMounted(() => {
+      Sortable.create(document.getElementById('menu-list')!, {
+        group: 'nested',
+        animation: 150,
+        onEnd(evt) {
+          const [removed] = menu.splice(evt.oldIndex!, 1);
+          menu.splice(evt.newIndex!, 0, removed);
+        },
+      });
+    });
+
+    return {
+      pages,
+      menu,
+      addPageToMenu,
+      removeMenuItem,
+    };
+  },
+});
+</script>
+
+<style scoped>
+/* TailwindCSS is already imported in the main.ts file */
 </style>
---
+++ src/components/MenuItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <li class="mb-2">
+    <div class="flex justify-between items-center p-2 bg-gray-100 rounded">
+      {{ item.name }}
+      <button @click="removeItem" class="text-red-500 ml-2">Remove</button>
+    </div>
+    <ul class="ml-4 mt-2" v-if="item.children.length">
+      <MenuItem
+        v-for="(child, index) in item.children"
+        :key="child.id"
+        :item="child"
+        :index="index"
+        @remove-item="removeChild"
+      />
+    </ul>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+interface MenuItem {
+  id: number;
+  name: string;
+  children: MenuItem[];
+}
+
+export default defineComponent({
+  name: 'MenuItem',
+  props: {
+    item: {
+      type: Object as () => MenuItem,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: ['remove-item'],
+  methods: {
+    removeItem() {
+      this.$emit('remove-item', this.index);
+    },
+    removeChild(childIndex: number) {
+      this.item.children.splice(childIndex, 1);
+    },
+  },
+});
+</script>
+
+<style scoped>
+/* Scoped styles for MenuItem */
+</style>
